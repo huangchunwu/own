375393e508401715ac47d9aa43e0ae3f7e6f70ce
huangchunwu <huangchunwu@tuniu.com>
2019-02-01 17:43:45 +0800

    加入threadLocal

--- src/main/java/cn/hcw/thread/ThreadLocalExample.java
+++ src/main/java/cn/hcw/thread/ThreadLocalExample.java
@@ -10,6 +10,13 @@ import java.util.concurrent.Executors;
  * 应用：可以用于单个线程的参数的存储，模板方法的上下文
  * 比如Java7中的SimpleDateFormat不是线程安全的，可以用ThreadLocal来解决这个问题：
  * 注意事项：防止使用线程池，导致内存泄露
+ *
+ * 每个线程维护了一个threadLocalMap，ThreadLocal作为弱引用的KEY，线程局部变量作为value
+ * 通常情况，当线程结束，ThreadLocal变量 作为 threadLocalMap的弱引用key，则在GC的时候就会被回收，
+ * 但是如果threadLocalMap的弱引用key的value还被强引用的话，此时即使key==null,threadLocalMap依然会内存泄露
+ * 当线程存活比较久，比如线程池，要记得手动清空threadLocalMap。
+ * 另外threadLocalMap的entry，当线程的局部变量值即value部分如果超过临界值，则会引发entry的GC
+ * 线程拥有的局部变量 如果超过容量的2/3（默认容量10个），则会涉及到ThreadLocalMap中Entry的回收；
  * Created by huangchunwu on 2019/2/1.
  */
 public class ThreadLocalExample {
